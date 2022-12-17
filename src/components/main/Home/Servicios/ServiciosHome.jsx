261659ec8095793a7dfbcb41f3e6bad364bb58ea
adrian411997 <adrian.mart41197@gmail.com>
2022-12-17 13:32:53 -0300

    partSecurityAdd

--- src/components/main/Home/Servicios/ServiciosHome.jsx
+++ src/components/main/Home/Servicios/ServiciosHome.jsx
@@ -4,11 +4,14 @@ import webpage from "../../../images/webpage1.png";
 import trabajo from "../../../images/trabajo.png";
 import nerd from "../../../images/nerd.png";
 import stonks from "../../../images/stonks.png";
+import security from "../../../images/security.png";
 
 const ServiciosHome = () => {
   const [imgContent, setImgContent] = useState("hidden-img-content");
   const [text, setText] = useState("hidden-text");
-  const handleAnimation = () => {
+  const [secur, setSecur] = useState("hidden-img-content");
+  const [textSecur, setTextSecur] = useState("hidden-text");
+  const handleAnimationWeb = () => {
     if (document.documentElement.scrollTop >= 500) {
       setImgContent("image-content");
       setText("text");
@@ -17,8 +20,18 @@ const ServiciosHome = () => {
       setText("hidden-text");
     }
   };
+  const handleAnimationSecur = () => {
+    if (document.documentElement.scrollTop >= 1100) {
+      setSecur("img-security-content");
+      setTextSecur("text-security-content");
+    } else {
+      setSecur("hidden-img-content");
+      setTextSecur("hidden-text");
+    }
+  };
   useEffect(() => {
-    window.addEventListener("scroll", handleAnimation);
+    window.addEventListener("scroll", handleAnimationWeb);
+    window.addEventListener("scroll", handleAnimationSecur);
   }, []);
   return (
     <div className="ServiciosPart">
@@ -60,8 +73,29 @@ const ServiciosHome = () => {
               No se preocupe, hacemos el trabajo duro por usted y le
               construiremos su sitio web a su medida para que resalte en
               cualquier sitio. Tambien le proporcionaremos un hosting con el
-              dominio a su nombre
+              dominio a su nombre.
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="security-service">
+          <div className={textSecur}>
+            <div className="text-title">
+              <h1>Cibersereguridad</h1>
+            </div>
+            <div className="parrafo-title">
+              <b>
+                ¿Preocupado de que su competencia ataque sus servicios web o
+                piratas informaticos estropeen la experiencia de sus usuarios?
+              </b>{" "}
+              Sabemos cuan vulnerable puede ser un sitio web ante ataques de
+              entes maliciosos, nosotros le proveeremos de las medidas de
+              seguridad para evitar estos contratiempos.
+            </div>
+          </div>
+          <div className={secur}>
+            <img src={security} alt="seguridad" width={550} height={350} />
           </div>
         </div>
       </div>
