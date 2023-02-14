f64656c62e8dca68fa702f331825f70707df7071
adrian411997 <adrian.mart41197@gmail.com>
2023-02-14 01:09:36 -0300

    transparent-header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,13 +10,16 @@ const Header = () => {
   const [isDesplegado, setIsDesplegado] = useState(false);
   const [changeBackGround, setChangeBackground] = useState("");
   const [color, setColor] = useState("#000");
+  const [justheader, setJustHeader] = useState("");
   const changeColor = () => {
     if (document.documentElement.scrollTop > 0) {
       setChangeBackground("sticky");
+      setJustHeader("black");
       setColor("#ffff");
     } else {
       setChangeBackground("");
       setColor("#000");
+      setJustHeader("");
     }
   };
   useEffect(() => {
@@ -52,53 +55,54 @@ const Header = () => {
     }
   };
   return (
-    <div className={`header ${changeBackGround} `}>
+    <div className={`header ${justheader}`}>
       <div className="pacentrar">
-        <div className="icon">
-          <Link to="/"></Link>
-          <Link to="/">
-            <img
-              className="icon-responsive"
-              src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1676164851/metrica/logofluor_nyajnu.png"
-              //src={"https://res.cloudinary.com/dni5cjwpu/image/upload/v1674247302/metrica/Recurso_4_aopmkj.png"}
-              alt="instagram"
-              width={150}
-              height={50}
-            />
-          </Link>
-        </div>
-        <div className="responseive-icon">
-          {isDesplegado ? (
-            <button onClick={classModified}>
-              <svg
-                viewBox="0 0 52.001 52.001"
-                width={20}
-                height={20}
-                fill={color}
-              >
-                <g>
+        <div className={`responsive-black-header ${changeBackGround}`}>
+          <div className={`icon`}>
+            <Link to="/">
+              <img
+                className="icon-responsive"
+                src="https://res.cloudinary.com/dni5cjwpu/image/upload/v1676164851/metrica/logofluor_nyajnu.png"
+                //src={"https://res.cloudinary.com/dni5cjwpu/image/upload/v1674247302/metrica/Recurso_4_aopmkj.png"}
+                alt="instagram"
+                width={150}
+                height={50}
+              />
+            </Link>
+          </div>
+          <div className="responseive-icon">
+            {isDesplegado ? (
+              <button onClick={classModified}>
+                <svg
+                  viewBox="0 0 52.001 52.001"
+                  width={20}
+                  height={20}
+                  fill={color}
+                >
                   <g>
-                    <path
-                      d="M47.743,41.758L33.955,26.001l13.788-15.758c2.343-2.344,2.343-6.143,0-8.486
+                    <g>
+                      <path
+                        d="M47.743,41.758L33.955,26.001l13.788-15.758c2.343-2.344,2.343-6.143,0-8.486
 			c-2.345-2.343-6.144-2.342-8.486,0.001L26,16.91L12.743,1.758C10.4-0.584,6.602-0.585,4.257,1.757
 			c-2.343,2.344-2.343,6.143,0,8.486l13.788,15.758L4.257,41.758c-2.343,2.343-2.343,6.142-0.001,8.485
 			c2.344,2.344,6.143,2.344,8.487,0L26,35.091l13.257,15.152c2.345,2.344,6.144,2.344,8.487,0
 			C50.086,47.9,50.086,44.101,47.743,41.758z"
-                    />
+                      />
+                    </g>
                   </g>
-                </g>
-              </svg>
-            </button>
-          ) : (
-            <button onClick={classModified}>
-              <svg width="25" height="25" viewBox="0 0 12 16">
-                <path
-                  fill={color}
-                  d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"
-                />
-              </svg>
-            </button>
-          )}
+                </svg>
+              </button>
+            ) : (
+              <button onClick={classModified}>
+                <svg width="25" height="25" viewBox="0 0 12 16">
+                  <path
+                    fill={color}
+                    d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"
+                  />
+                </svg>
+              </button>
+            )}
+          </div>
         </div>
         <div className={`pest ${addClass}`}>
           <div className="enlaces">
